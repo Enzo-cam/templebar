db3803c7065a028d184fb2855b32b2ce5c6f16f4
EnzoCam7 <camerac09@gmail.com>
2023-01-17 18:23:03 -0300

    Principal configuration of project

---
+++ prisma/migrations/20230117211217_orden/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Orden" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "fecha" TEXT NOT NULL,
+    "total" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "Orden_pkey" PRIMARY KEY ("id")
+);
