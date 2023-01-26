742ff8b42a204040203d01b779fa0f150b50b2fa
EnzoCam7 <camerac09@gmail.com>
2023-01-26 19:09:56 -0300

    .

---
+++ src/pages/total.js
@@ -0,0 +1,13 @@
+import Layout from "layout/Layout"
+
+function Total() {
+  return (
+    <Layout pagina="Total">
+      <h1 className="text-5xl font-medium">Total a pagar:</h1>
+      <p className="text-2xl my-2">Abona tu pedido</p>
+
+    </Layout>
+  )
+}
+
+export default Total
