d043d7f401424ae59c9be4ba146ff5fa3cc7c68c
EnzoCam7 <camerac09@gmail.com>
2023-01-31 00:08:04 -0300

    Reset app

--- Context/TempleProvider.jsx
+++ Context/TempleProvider.jsx
@@ -64,8 +64,17 @@ const TempleProv = ({children}) =>{
         e.preventDefault()
 
         try {
-            const {data} =  await axios.post('/api/ordenes', {pedido, nombre, total, fecha : Date.now().toString()})
-            console.log(data)
+            await axios.post('/api/ordenes', {pedido, nombre, total, fecha : Date.now().toString()})
+            // Reiniciar la app a 0.
+            setCategorias(categorias[0])
+            setPedido([])
+            setNombre('')
+            setTotal(0)
+
+            toast.success('Pedido realizado correctamente.')
+            setTimeout(() => {
+                router.push('/')
+            }, 2000);
         } catch (error) {
             console.log(error)
         }
